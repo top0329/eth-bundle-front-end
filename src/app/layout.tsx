b2d35256de3be9fe4bf0ab8d826569ab27ebfac8
top0329 <top000329@gmail.com>
2024-06-14 04:46:42 +1000

    fix: add cookie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+        <Providers cookie={cookie}>
           <div
             style={{
               display: 'flex',
